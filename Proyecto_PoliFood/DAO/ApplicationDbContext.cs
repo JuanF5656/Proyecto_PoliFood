34f3c45ef0d243f4c7c2e3dddcb272bca8c23231
JuanF5656 <juan.atehortua56@eia.edu.co>
2026-03-11 09:08:53 -0500

    ApplicationDbContext con clases de Polifood

--- Proyecto_PoliFood/DAO/ApplicationDbContext.cs
+++ Proyecto_PoliFood/DAO/ApplicationDbContext.cs
@@ -11,9 +11,15 @@ namespace Proyecto_PoliFood.DAO
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<Eventos> Events { get; set; }
-        public DbSet<Boleta> Tickets { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+
 
     }
 }
