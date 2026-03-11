01751802e1e31c5f570bd765bcc5f81e87675587
JuanF5656 <juan.atehortua56@eia.edu.co>
2026-03-11 08:55:41 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_PoliFood/DAO/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Proyecto_PoliFood.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Proyecto_PoliFood.DAO
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Eventos> Events { get; set; }
+        public DbSet<Boleta> Tickets { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
